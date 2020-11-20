7adbe5a6f1a10cbe04f9c0b8ad5fc4207090b327
Amy Paschke <pasch049@umn.edu>
2020-11-20 09:39:58 -0600

    added a few pieces to index.js to include the questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,52 @@
+const fs = require("fs");
+const questions = require("inquirer");
+
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    message: "What is your README title?",
+    title: `${process.argv[2]}`,
+  },
+  {
+    type: "input",
+    message: "What is the description of your README?",
+    text: `${process.argv[3]}`,
+  },
+  {
+    type: "input",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username",
+    },
+    {
+      type: "password",
+      message: "What is your password?",
+      name: "password",
+    },
+    {
+      type: "password",
+      message: "Re-enter password to confirm:",
+      name: "confirm",
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log("Success!")
+      : console.log("You forgot your password already?!")
+  );
