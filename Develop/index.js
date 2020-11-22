43818e784269172b8ae60757160ab76e56be87f2
Amy Paschke <pasch049@umn.edu>
2020-11-21 19:36:36 -0600

    added base info to markdown file and added more fields to index js

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const inquirer = require("inquirer");
 const questions = require("inquirer");
 
 // array of questions for user
@@ -6,47 +7,70 @@ const questions = [
   {
     type: "input",
     message: "What is your README title?",
-    title: `${process.argv[2]}`,
+    name: "title",
   },
   {
     type: "input",
-    message: "What is the description of your README?",
-    text: `${process.argv[3]}`,
+    message: "Write a brief description for your README.",
+    name: "description",
   },
   {
     type: "input",
+    message: "Please write out the installation instructions.",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Please write out the useage information.",
+    name: "useage",
+  },
+  {
+    type: "input",
+    message: "Any contribution guidelines?",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "List any test instructions.",
+    name: "testInstructions",
+  },
+  {
+    type: "checkbox",
+    message: "Please select which licenses to apply:",
+    name: "license",
+    choices: [
+      {
+        name: "MIT",
+        name: "CreativeCommons",
+        name: "GNU",
+        name: "Mozilla",
+      },
+    ],
+  },
+  {
+    type: "input",
+    message: "Enter your GitHub username",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your preferred email address?",
+    name: "email",
   },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const fileName = "README.md";
+  let;
+}
 
 // function to initialize program
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(() => {
+    writeToFile();
+  });
+}
 
 // function call to initialize program
 init();
-
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your user name?",
-      name: "username",
-    },
-    {
-      type: "password",
-      message: "What is your password?",
-      name: "password",
-    },
-    {
-      type: "password",
-      message: "Re-enter password to confirm:",
-      name: "confirm",
-    },
-  ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log("Success!")
-      : console.log("You forgot your password already?!")
-  );
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,45 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let markdown = `# ${data.title}\n
+  ${data.description}\n
+  ## Licenses\n
+  ${data.license}\n
+  ## Installation\n
+  ${data.installation}\n
+  ## Useage\n
+  ${data.useage}\n
+  ## Contribution\n
+  ${data.contribution}\n
+  ## Test Instructions\n
+  ${data.testInstructions}\n
+  ##Questions?\n
+  My GitHub username is [${data.github}](https://www.github.com/${data.github}).
+  Email me at [${data.email}](mailto:${data.email}).
+  
+  `
 
-`;
+;
 }
 
 module.exports = generateMarkdown;
+
+
+# Professional README Generator
+
+This generator utilizes node.js
+
+## Motivation
+
+New to node.js, this project tests the functionality of my node.js skills and use of the terminal.
+
+## Screenshots
+
+## Code Example
+
+## Features
+
+## License
+
+MIT License
+
+## Credits
